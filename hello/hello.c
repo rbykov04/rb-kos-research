3ae48171f41eaa8c1fb931eded101659720ab985
rbykov <rbykov04@gmail.com>
2025-02-20 10:34:19 +0300

    Make hello struct to be flat

---
+++ hello/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    fprintf(stderr,"Hello world from Kaspersky OS!\n");
+    return EXIT_SUCCESS;
+}
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+# prepare build direcrory
+cd hello && ./cross-build.sh
