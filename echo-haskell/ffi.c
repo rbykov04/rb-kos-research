8042223f759a2465fddde5e92b505c548cad9cff
rbykov <rbykov04@gmail.com>
2025-05-30 15:05:14 +0300

    echo/haskell replace struct to buffer to prepare move code to haskell

--- echo-haskell/ffi.c
+++ echo-haskell/ffi.c
@@ -31,44 +31,21 @@ enum {
     Echo_res_handles = 0,
     Echo_err_handles = 0,
 };
-typedef struct __nk_packed EchoReq {
-            __nk_alignas(8)
-            struct nk_message base_;
-            __nk_alignas(4) nk_ptr_t value;
-        } EchoReq;
-static_assert(sizeof(EchoReq) == 32, "bad_Echo_Echo_req_size");
-static_assert(nk_offsetof(EchoReq, base_) == 0, "bad_Echo_Echo_req_base__offset");
-static_assert(nk_offsetof(EchoReq, value) == 24, "bad_Echo_Echo_req_value_offset");
-
-#pragma pack(push, 8) /* Echo_Echo_res */
-typedef struct Echo_Echo_err {
-            __nk_alignas(8)
-            struct nk_message base_;
-        } Echo_Echo_err;
-static_assert(sizeof(Echo_Echo_err) == 24, "bad_Echo_Echo_err_size");
-static_assert(nk_offsetof(Echo_Echo_err, base_) == 0, "bad_Echo_Echo_err_base__offset");
-typedef struct Echo_Echo_res {
-            union {
-                struct {
-                    __nk_alignas(8)
-                    struct nk_message base_;
-                };
-                struct {
-                    __nk_alignas(8)
-                    struct nk_message base_;
-                } res_;
-                struct Echo_Echo_err err_;
-            };
-        } Echo_Echo_res;
-static_assert(sizeof(Echo_Echo_res) == 24, "bad_Echo_Echo_res_size");
-static_assert(nk_offsetof(Echo_Echo_res, base_) == 0, "bad_Echo_Echo_res_base__offset");
-#pragma pack(pop) /* Echo_Echo_res */
-
-
-
 
 #define MESSAGE_SIZE 100
 
+void fillEnvelope(nk_message* envelope
+                  , size_t size
+                  , nk_iid_t riid
+                  , nk_mid_t mid
+                  , uint32_t ncaps)
+{
+    nk_message_set_size(envelope, size);
+    nk_message_make_req(envelope);
+    nk_message_set_iid(envelope, riid);
+    nk_message_set_mid(envelope, mid);
+    nk_message_set_ncaps(envelope, ncaps);
+}
 
 int sendToHello( Handle handle
                , nk_iid_t riid
@@ -81,28 +58,33 @@ int sendToHello( Handle handle
     assert(riid != INVALID_RIID);
     assert(reqArena != NULL);
 
-    EchoReq req;
-    Echo_Echo_res res;
+    char request[32] = {0};
+    fillEnvelope ((nk_message*) &request
+                 , sizeof(request)
+                 , riid
+                 , Echo_Echo_mid
+                 , Echo_Echo_req_handles);
+
+    nk_ptr_t   *value    = (nk_ptr_t*) &request[24];
     nk_arena_store(nk_char_t
                   , reqArena
-                  , &(req.value)
+                  , value
                   , message
                   , size );
 
+    char response[24] = {0};
 
-    rtl_printf("Echo mid = %d \n", Echo_Echo_mid);
-    nk_req_reset(&req);
-    nk_msg_set_method_id(&req, riid, Echo_Echo_mid);
-    nk_msg_set_ncaps(&req, Echo_Echo_req_handles);
-    nk_req_reset(&res);
-    nk_msg_set_method_id(&res, riid, Echo_Echo_mid);
-    nk_msg_set_ncaps(&res, Echo_Echo_res_handles);
+    fillEnvelope ((nk_message*) &response
+                 , sizeof(response)
+                 , riid
+                 , Echo_Echo_mid
+                 , Echo_Echo_res_handles);
 
     SMsgHdr msgReq;
     SMsgHdr msgRes;
 
-    PackOutMsg (&msgReq, &req.base_, reqArena);
-    PackInMsg  (&msgRes, &res.base_, NULL);
+    PackOutMsg (&msgReq, (nk_message*) &request, reqArena);
+    PackInMsg  (&msgRes, (nk_message*) &response, NULL);
 
     Retcode rc = Call (handle, &msgReq , &msgRes);
     if (rc != rcOk) {
